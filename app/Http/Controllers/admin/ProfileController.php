988da8f712b1f4a373f4e87ebf12604b029b4a9b
”Yoshiro <toyoura226@yahoo.co.jp>
2019-10-03 04:23:11 +0000

    first commit

---
+++ app/Http/Controllers/admin/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+      return view('admin.profile.create');
+    }
+    
+    public function create()
+    {
+      return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+      return view('admin.profile.create');
+    }
+    
+    public function update()
+    {
+      return redirect('admin/profile/edit');
+    }
+}
+
+
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('profile/create','ProfileController@add');
+});
+
+Route::group(['prefix' => 'edit'], function() {
+  Route::get('profile/create','ProfileController@edit');
+});
